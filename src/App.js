a54538c57220760c0a647fa9449c4371247a5a5f
CodeWithSam <codewithsam@gmail.com>
2019-12-28 05:44:20 +0530

    Gallerina | Initial Setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Gallerina from './Gallerina';
+import GallerinaImage from './GallerinaImage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      index: 0
+    }
+    this.imgs = [
+      {
+        img: "https://i.picsum.photos/id/237/400/200.jpg"
+      },
+      {
+        img: "https://i.picsum.photos/id/866/400/200.jpg"
+      },
+      {
+        img: "https://i.picsum.photos/id/93/400/200.jpg?grayscale"
+      },
+      {
+        img: "https://i.picsum.photos/id/1/400/200.jpg?grayscale"
+      },
+      {
+        img: "https://i.picsum.photos/id/2/400/200.jpg?grayscale"
+      }
+    ];
+  }
+  render(){
+    const {state: {index}, imgs} = this;
+    return(
+      <Gallerina startingIndex={index}>
+        {imgs.map((image, i) => <GallerinaImage image={image} key={i} />)}
+      </Gallerina>
+    )
+  }
 }
-
-export default App;
---
+++ src/GallerinaImage.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class GallerinaImage extends Component{
+    render(){
+        return(
+            <div>asdsad</div>
+        )
+    }
+}
