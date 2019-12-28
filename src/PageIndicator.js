035259250812770851cd9d89b95b0307f2783d9b
CodeWithSam <codewithsam@gmail.com>
2019-12-28 21:02:50 +0530

    PageIndicator | added file :P

---
+++ src/PageIndicator.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function PageIndicator({length, styles, selectedIndex}){
+    let elems = [];
+    for(let i=0;i<length;i++){
+        elems.push(<span key={i} style={{
+            backgroundColor: selectedIndex===i ? "#88c6e3" : "#eee",
+            border: "1px solid #444",
+            borderRadius: "50%",
+            width: "10px",
+            height: "10px",
+            marginRight: "2px",
+            ...styles
+        }}></span>)
+    }
+    return(
+        <div style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center"
+        }}>
+            {elems}
+        </div>
+    )
+}
